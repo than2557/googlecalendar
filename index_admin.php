db77e4f721a7acaf045a525528ae4b798cbca6e6
nam <than2557@gmail.com>
2020-08-26 15:34:42 +0700

    index admin

--- index_admin.php
+++ index_admin.php
@@ -475,9 +475,9 @@ $( document ).ready(function() {
     <nav class="navbar navbar-expand navbar-light bg-white topbar mb-4 static-top shadow">
 
       <!-- Sidebar Toggle (Topbar) -->
-      <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
+      <!-- <button id="sidebarToggleTop" class="btn btn-link d-md-none rounded-circle mr-3">
         <i class="fa fa-bars"></i>
-      </button>
+      </button> -->
 
       <!-- Topbar Search -->
    
